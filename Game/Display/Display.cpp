85d32ad1e2ff2b871742ffd64ffd192f8b2b25d6
Khai <zekhai@mail.fresnostate.edu>
2019-11-30 00:25:33 -0800

    Display onRender might work?

--- Game/Display/Display.cpp
+++ Game/Display/Display.cpp
@@ -25,9 +25,40 @@ void Display::ClearWalls() {
 }
 
 void Display::OnRender() {
+    int size = this->window->gridSizeY * this->window->gridSizeX;
+    std::vector<SDL_Rect*>* rects = new std::vector<SDL_Rect*>();
+    int currWall = 0;
     
+    for (int i = 0; i < size; ++i) {
+        int x = this->walls->at(i) % this->window->gridSizeX;
+        int y = this->walls->at(i) / this->window->gridSizeY;
+        rects->push_back(new SDL_Rect{x, y, this->window->bitSize, this->window->bitSize});
+        if (i == this->walls->at(currWall)) {
+            this->window->Draw(this->wallTexture, rects->at(i), rects->at(i));
+            ++currWall;
+        }
+        else {
+            this->window->Draw(this->grassTexture, rects->at(i), rects->at(i));
+        }
+    }
+    
+    for (int i = 0; i < this->entities->size(); ++i) {
+        this->entities->at(i)->OnRender();
+    }
+    
+    for (int i = 0; i < rects->size(); ++i) {
+        delete rects->at(i);
+    }
+    rects->clear();
+    rects->shrink_to_fit();
+    delete rects;
 }
 
 Display::~Display() {
-    
+    this->ClearWalls();
+    delete this->walls;
+    SDL_DestroyTexture(levelMsgTexture);
+    SDL_DestroyTexture(grassTexture);
+    SDL_DestroyTexture(wallTexture);
+    levelMsgTexture = grassTexture = wallTexture = nullptr;
 }
