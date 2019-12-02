f5bdf2ce180899510ed1c59e8351cfe96b3e0b3e
Khai <zekhai@mail.fresnostate.edu>
2019-12-01 22:32:30 -0800

    Modified OnRender on Display

--- Game/Display/Display.cpp
+++ Game/Display/Display.cpp
@@ -26,32 +26,27 @@ void Display::ClearWalls() {
 
 void Display::OnRender() {
     int size = this->window->gridSizeY * this->window->gridSizeX;
-    std::vector<SDL_Rect*>* rects = new std::vector<SDL_Rect*>();
+    SDL_Rect* currRect = nullptr;
     int currWall = 0;
     
     for (int i = 0; i < size; ++i) {
-        int x = this->walls->at(i) % this->window->gridSizeX;
-        int y = this->walls->at(i) / this->window->gridSizeY;
-        rects->push_back(new SDL_Rect{x, y, this->window->bitSize, this->window->bitSize});
+        int x = i % this->window->gridSizeX;
+        int y = i / this->window->gridSizeY;
+        currRect = new SDL_Rect{x, y, this->window->bitSize, this->window->bitSize;
         if (i == this->walls->at(currWall)) {
-            this->window->Draw(this->wallTexture, rects->at(i), rects->at(i));
+            this->window->Draw(this->wallTexture, currRect, currRect);
             ++currWall;
         }
         else {
-            this->window->Draw(this->grassTexture, rects->at(i), rects->at(i));
+            this->window->Draw(this->grassTexture, currRect, currRect);
         }
+        delete currRect;
+        currRect = nullptr; 
     }
     
     for (int i = 0; i < this->entities->size(); ++i) {
         this->entities->at(i)->OnRender();
     }
-    
-    for (int i = 0; i < rects->size(); ++i) {
-        delete rects->at(i);
-    }
-    rects->clear();
-    rects->shrink_to_fit();
-    delete rects;
 }
 
 Display::~Display() {
